1912bb534a03c69b1adad3f177337acc6ee98021
Rakesh550 <raki55077@gmail.com>
2022-07-18 17:56:22 +0300

    added local files

---
+++ README.md
@@ -0,0 +1,7 @@
+# PHP-Practice
+
+
+steps.
+1- Create a repo directory
+2- git clone the project
+3. use command :  docker-compose up --build -d
---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+   //database connection config
+        $hostname="db";
+        $db_user="root";
+        $db_pass="root";
+        $db_name="mysql";
+
+        //connecting to database
+        $connection=mysqli_connect($hostname, $db_user, $db_pass, $db_name);
+        if(mysqli_connect_errno()){
+                die("Error connecting to Database");
+        }
+
+        //adding new visitor
+        $visitor_ip=$_SERVER['REMOTE_ADDR'];
+
+        //checking if the visitor is unique
+        $query="SELECT * FROM counter_table WHERE ip_address='$visitor_ip'";
+        $result=mysqli_query($connection, $query);
+
+        //checking query error
+        if(!$result){
+                die("Retriving Query Eroor<br>".$query);
+        }
+        $total_visitors=mysqli_num_rows($result);
+        if($total_visitors<1){
+                $query="INSERT INTO counter_table(ip_address) VALUES('$visitor_ip')";
+                $result=mysqli_query($connection, $query);
+        }
+
+
+        //retriving existing visitors
+        $query="SELECT * FROM counter_table";
+        $result=mysqli_query($connection, $query);
+
+        //checking query error
+        if(!$result){
+                die("Retriving Query Eroor<br>".$query);
+        }
+        $total_visitors=mysqli_num_rows($result);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+        <title></title>
+        <style type="text/css">
+                .wrapper{
+                        height: 300px;
+                        width: 300px;
+                        background-color: skyblue;
+                        margin: auto;
+                        text-align: center;
+                        border: 1px solid white;
+                        box-shadow: 2px 2px 10px gray;
+                }
+                h1{
+                        background-color: mediumseagreen;
+                        color: white;
+                        border-bottom: 2px solid white;
+                }
+                h3{
+                        font-size: 5em;
+                }
+                h1,h3{
+                        padding: 20px;
+                        margin: 0px;
+                }
+        </style>
+</head>
+<body>
+        <div class="wrapper">
+                <h1>
+                        Unique test
+                </h1>
+                <h3><?php echo $total_visitors; ?></h3>
+        </div>
+</body>
+</html>
