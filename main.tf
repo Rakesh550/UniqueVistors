b88a6fb612e38e5a05d0150a343b2c3302b18ce8
Rakesh550 <63194892+Rakesh550@users.noreply.github.com>
2022-07-20 10:42:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,95 +18,82 @@ provider "azurerm" {
     tenant_id = var.tenant_id
 }
 
-resource "azurerm_resource_group" "pocrglabel" {
-    name = "pocrg"
-    location = "West US"
-  
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "pocvnetlabel" {
-    name = "pocvnet"
-    resource_group_name = azurerm_resource_group.pocrglabel.name
-    location = azurerm_resource_group.pocrglabel.location
-    address_space = [ "10.70.0.0/16" ]
-
-  
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "websubnet1" {
-    name = "websubnet"
-    resource_group_name = azurerm_resource_group.pocrglabel.name
-    virtual_network_name = azurerm_virtual_network.pocvnetlabel.name
-    address_prefixes = [ "10.70.1.0/24" ]
-
-  
-}
-
-resource "azurerm_subnet" "appsubnet1" {
-    name = "appsubnet"
-    resource_group_name = azurerm_resource_group.pocrglabel.name
-    virtual_network_name = azurerm_virtual_network.pocvnetlabel.name
-    address_prefixes = [ "10.70.2.0/24" ]
-
-  
-}
-
-
-resource "azurerm_subnet" "dbsubnet1" {
-    name = "dbsubnet"
-    resource_group_name = azurerm_resource_group.pocrglabel.name
-    virtual_network_name = azurerm_virtual_network.pocvnetlabel.name
-    address_prefixes = [ "10.70.3.0/24" ]
-
-  
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 # Public ip address for Web server
 resource "azurerm_public_ip" "webpublicip1" {
     name = "webpublicip"
-    resource_group_name = azurerm_resource_group.pocrglabel.name
-    location = azurerm_resource_group.pocrglabel.location
+    resource_group_name = azurerm_resource_group.example.name
+    location = azurerm_resource_group.example.location
     allocation_method = "Static"
-  
+
 }
 
-# NIC for web server
-resource "azurerm_network_interface" "webnic1" {
-    name = "webnic"
-    location = azurerm_resource_group.pocrglabel.location
-    resource_group_name = azurerm_resource_group.pocrglabel.name
-    ip_configuration {
-      name = "public"
-      subnet_id = azurerm_subnet.websubnet1.id
-      private_ip_address_allocation = "Dynamic"
-      public_ip_address_id = azurerm_public_ip.webpublicip1.id
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
-    }
-  
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.webpublicip1.id
+  }
 }
 
-resource "azurerm_linux_virtual_machine" "webserver1" {
-    name = "webserver"
-    resource_group_name = azurerm_resource_group.pocrglabel.name
-    location = azurerm_resource_group.pocrglabel.location
-    size = "Standard_F2"
-    admin_username = "adminuser"
-    network_interface_ids = [ 
-        azurerm_network_interface.webnic1.id 
-        ]
-    admin_ssh_key {
-      username = "adminuser"
-      public_key = file("~/.ssh/id_rsa.pub")
-    }
-    os_disk {
-      caching = "ReadWrite"
-      storage_account_type = "Standard_LRS"
-    }
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
 
-    source_image_reference {
-      publisher = "Canonical"
-      offer = "UbuntuServer"
-      sku = "18.04-LTS"
-      version = "latest"
-    }
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
 }
+
